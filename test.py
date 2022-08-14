6a1f2c5438028311981578be98b9845d0562e067
colakhasanfurkan <110482667+colakhasanfurkan@users.noreply.github.com>
2022-08-14 16:34:20 +0300

    update

--- test.py
+++ test.py
@@ -1,52 +1,11 @@
-import pandas as pd
-import numpy as np
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt 
 
-df = pd.DataFrame({
-    'Population': [35.467, 63.951, 80.94 , 60.665, 127.061, 64.511, 318.523],
-    'GDP': [
-        1785387,
-        2833687,
-        3874437,
-        2167744,
-        4602367,
-        2950039,
-        17348075
-    ],
-    'Surface Area': [
-        9984670,
-        640679,
-        357114,
-        301336,
-        377930,
-        242495,
-        9525067
-    ],
-    'HDI': [
-        0.913,
-        0.888,
-        0.916,
-        0.873,
-        0.891,
-        0.907,
-        0.915
-    ],
-    'Continent': [
-        'America',
-        'Europe',
-        'Europe',
-        'Europe',
-        'Asia',
-        'Europe',
-        'America'
-    ]
-}, columns=['Population', 'GDP', 'Surface Area', 'HDI', 'Continent'])
-df.index = [
-    'Canada',
-    'France',
-    'Germany',
-    'Italy',
-    'Japan',
-    'United Kingdom',
-    'United States',
-]
-print(df.loc["Canada"])
+
+x = np.arange(-10,11)
+
+plt.figure(figsize=(12,6))
+plt.title("Nice Shit")
+plt.plot(x,x**2)
+plt.plot(x,-1*(x**2))
